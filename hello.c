9490b3f128a349adc40f2224ed23cefd373e72fb
gitusername <github@mailna.biz>
2017-11-18 14:58:51 +0200

    first work

--- hello.c
+++ hello.c
@@ -1,16 +1,71 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <getopt.h>
 
-int main(int agrc, char argv[])
+struct option longopts[] =
 {
+	{"greeting",	required_argument,	NULL,	'g'},
+	{"help",	no_argument,		NULL,	'h'},
+	{"traditional",	no_argument,		NULL,	't'},
+	{"version",	no_argument,		NULL,	'v'},
+	{NULL, 		0, 			NULL, 	 0 }
+};
 
+static const char version[] = "1.0";
+
+void print_help();
+void print_version();
+
+int main(int argc, char *argv[])
+{
+	int optc;
+	int lose = 0;
+	const char *greeting_msg;
+
+	greeting_msg = "Hello World!";
+
+	while(( optc = getopt_long(argc, argv, "g:htv", longopts, NULL)) != -1)
+	{
+		switch(optc)
+		{
+			case 'v':
+				print_version();
+				exit(EXIT_SUCCESS);
+				break;
+      			case 'g':
+				greeting_msg = optarg;
+				break;
+      			case 'h':
+				print_help ();
+				exit(EXIT_SUCCESS);
+				break;
+      			case 't':
+				greeting_msg = "hello, world";
+				break;
+      			default:
+				lose = 1;
+				break;
+		}
+	}
+
+	if(lose || optind < argc)
+	{
+		printf("error\n");
+		print_help();
+		exit(EXIT_SUCCESS);
+	}
+
+	printf("%s\n", greeting_msg);
 }
 
-static void print_help()
+void print_help()
 {
-
+	printf("Usage: hello [OPTION]...\n");
+	printf("Print a friendly, customizable greeting.\n");
 }
 
-static void print_version()
+void print_version()
 {
-
+	printf("hello version %s\n", version);
 }
